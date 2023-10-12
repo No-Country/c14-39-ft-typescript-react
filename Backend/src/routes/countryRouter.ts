df56f8038448b23deeebce78aa5d2267d243bb15
Luis Angel <lguzman.58erb@outlook.com>
2023-10-12 09:46:23 -0500

    avance user

--- Backend/src/routes/countryRouter.ts
+++ Backend/src/routes/countryRouter.ts
@@ -4,28 +4,29 @@ import Country from '../models/Country'
 const countryRouter = express.Router()
 
 countryRouter.route('/').
-  get( async (_, res) => {
+  get(async (_, res) => {
     try {
-      res.send('hola ruta country')
+      const countries = await Country.find().select('-users')
+      res.status(201).json({ countries })
     } catch (error) {
       throw error
     }
   })
-  .post( async (req, res) => {
-  try {
-    const { name, image } = req.body
-    const newCountry = new Country({
-      name,
-      image
-    })
+  .post(async (req, res) => {
+    try {
+      const { name, image } = req.body
+      const newCountry = new Country({
+        name,
+        image
+      })
 
-    await newCountry.save()
+      await newCountry.save()
 
-    res.status(201).json({ country: newCountry, message: "country created" })
-  } catch (error: any) {
-    res.status(400).json({ error: error.message })
-  }
-})
+      res.status(201).json({ country: newCountry, message: "country created" })
+    } catch (error: any) {
+      res.status(400).json({ error: error.message })
+    }
+  })
 
 
 export default countryRouter
--- Backend/src/routes/userRouter.ts
+++ Backend/src/routes/userRouter.ts
@@ -1,13 +1,48 @@
 import express from 'express'
 import User from '../models/User'
+import County from '../models/Country'
 
 const userRouter = express.Router()
 
 userRouter
   .route('/')
-  .get(async (req, res) => {
+  .get(async (_, res) => {
     try {
-      res.send('hola ruta users')
+      const users = await User.find()
+      res.status(201).json({ users })
+    } catch (error) {
+      throw error
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const {
+        name,
+        lastname,
+        email,
+        password,
+        city,
+        address,
+        image,
+        countryId
+      } = req.body
+
+      const country = await County.findById(countryId)
+
+      const newUser = new User({
+        name,
+        lastname,
+        email,
+        password,
+        city,
+        address,
+        image,
+        country: country?._id,
+      })
+
+      await newUser.save()
+      res.status(201).json({ user: newUser, message: "user created" })
+
     } catch (error) {
       throw error
     }
