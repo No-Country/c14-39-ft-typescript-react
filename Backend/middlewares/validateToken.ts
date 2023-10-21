6cde0b46aaee1d42304bc49962fff66bdb6e0ac8
taypedev <juan.taype@paku.pe>
2023-10-21 14:43:04 -0500

    reservation v1.0.0

--- Backend/middlewares/validateToken.ts
+++ Backend/middlewares/validateToken.ts
@@ -1,24 +1,31 @@
-import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const { TOKEN_SECRET } = process.env;
+const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
 if (!TOKEN_SECRET) {
-    throw new Error('TOKEN_SECRET is not defined in the environment variables');
+  throw new Error("TOKEN_SECRET is not defined in the environment variables");
 }
 
-export const authRequired = (req: Request, res: Response, next: NextFunction) => {
-    
-    const token: string | undefined = req.cookies.token;
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
+export const authRequired = (
+  req: Request | any,
+  res: Response,
+  next: NextFunction
+) => {
+  const token: string | undefined = req.cookies.token;
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-    jwt.verify(token, TOKEN_SECRET as string, (err: jwt.JsonWebTokenError | null, user: any ) => {
-        if (err) return res.status(403).json({ message: "Invalid token" });
+  jwt.verify(
+    token,
+    TOKEN_SECRET as string,
+    (err: jwt.JsonWebTokenError | null, user: any) => {
+      if (err) return res.status(403).json({ message: "Invalid token" });
 
-        req.userId = user;
-        next();
-    });
-}
+      req.userId = user;
+      next();
+    }
+  );
+};
