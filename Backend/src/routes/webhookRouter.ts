794f57059c2b4b239797e04dce9a868fcce06469
Luis Angel <lguzman.58erb@outlook.com>
2023-10-25 15:17:09 -0500

    changes webhook

--- Backend/src/routes/webhookRouter.ts
+++ Backend/src/routes/webhookRouter.ts
@@ -20,28 +20,6 @@ webhookRouter
         await controller.createOrder(dataId)
         return res.status(HttpCodes.CODE_SUCCESS_CREATED).end()
       }
-      //   const paymented = new Payment(client)
-      //   const data = await paymented.get({
-      //     id: payment["data.id"] as string | number,
-      //   })
-
-      //   // verificar el estado del pago
-      //   if (data && data.status === "approved") {
-      //     // crear la orden
-      //     const order = new Order({
-      //       order_id: data.id as string | number,
-      //       fecha: data.metadata.fecha as string,
-      //       hora: data.metadata.hora as string,
-      //       currency: data.currency_id as string,
-      //       precio: data.transaction_amount as number,
-      //       user_id: data.metadata.user_id as string,
-      //       camp_id: data.metadata.camp_id as string,
-      //       sc_id: data.metadata.sc_id as string,
-      //     })
-
-      //     await order.save()
-      //   }
-      // }
 
       return res.status(HttpCodes.CODE_BAD_REQUEST).json({
         message: "Error en la creacion del Order",
