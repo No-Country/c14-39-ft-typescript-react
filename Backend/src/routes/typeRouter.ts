7c14d148c6ca9507f1ca326beec17007a902a923
taypedev <juan.taype@paku.pe>
2023-10-26 09:46:11 -0500

    deploy backend in vercel V1.0.2

--- Backend/src/routes/typeRouter.ts
+++ Backend/src/routes/typeRouter.ts
@@ -1,37 +1,38 @@
-import express from 'express'
-import { Type } from '../models'
-import { HttpCodes } from '../utils/HTTPCodes.util'
-import { validateSchema } from '../../middlewares/validatorMiddleware'
-import { TypeUserSchemaValidator } from '../models/schemas/schemas.zod'
+import express from "express";
+import { Type } from "../models";
+import { HttpCodes } from "../utils/HTTPCodes.util";
+import { validateSchema } from "../middlewares/validatorMiddleware";
+import { TypeUserSchemaValidator } from "../models/schemas/schemas.zod";
 
-const TypeRouter = express.Router()
+const TypeRouter = express.Router();
 
-TypeRouter.route('/').
-  get(async (_, res) => {
+TypeRouter.route("/")
+  .get(async (_, res) => {
     try {
-      const types = await Type.find().select('-users')
-      return res.status(HttpCodes.CODE_SUCCESS).json({ types })
+      const types = await Type.find().select("-users");
+      return res.status(HttpCodes.CODE_SUCCESS).json({ types });
     } catch (error) {
       return res.status(HttpCodes.CODE_NOT_FOUND).json({
         message: `${error}`,
-      })
+      });
     }
   })
   .post(validateSchema(TypeUserSchemaValidator), async (req, res) => {
     try {
       const newType = new Type({
-        ...req.body
-      })
+        ...req.body,
+      });
 
-      await newType.save()
+      await newType.save();
 
-      return res.status(HttpCodes.CODE_SUCCESS_CREATED).json({ type: newType, message: "Type created" })
+      return res
+        .status(HttpCodes.CODE_SUCCESS_CREATED)
+        .json({ type: newType, message: "Type created" });
     } catch (error) {
       return res.status(HttpCodes.CODE_BAD_REQUEST).json({
         message: `${error}`,
-      })
+      });
     }
-  })
+  });
 
-
-export default TypeRouter
+export default TypeRouter;
