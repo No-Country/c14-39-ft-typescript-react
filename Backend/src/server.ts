e0633ffeabae9b5cc3882de34b2c2826ec1b18bc
Kevin Defalco <kevindefalco@gmail.com>
2023-10-30 11:11:26 -0300

    conectando back y front

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -17,7 +17,7 @@ import cors from "cors";
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors( {origin: "http://localhost:5173", credentials: true} ));
+app.use(cors( {origin: ["http://localhost:5173", "https://app.reservatucancha.vercel.app/"], credentials: true} ));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
