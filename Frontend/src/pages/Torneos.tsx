54a07bde6859a9c04d4d1e889bc29a485fd405ab
cavera <cavera.de@gmail.com>
2023-10-30 21:55:36 -0500

    new public routes

---
+++ Frontend/src/pages/Torneos.tsx
@@ -0,0 +1,21 @@
+import { SectionTitle } from '../components/SectionTitle'
+
+const Torneos = () => {
+  return (
+    <section className='wrapper animate-fade-in'>
+      <SectionTitle title='¡Próximamente: Sección de Torneos!' />
+      <article className='max-w-xl pb-4 mx-auto my-12 '>
+        <p>Nos complace anunciar que muy pronto lanzaremos una sección especializada en Torneos en "Reserva tu Campo". Esta sección te permitirá explorar y registrarte en torneos de fútbol organizados por establecimientos confiables en tu área.</p>
+        <h2 className='pt-12 pb-4 text-2xl text-center font-display'>Explora y Participa</h2>
+        <p>Descubre una amplia gama de torneos locales o regionales, cada uno con su propio conjunto de reglas y recompensas. Tanto si eres un jugador ávido como si simplemente disfrutas de una competencia amistosa, encontrarás el torneo perfecto que se adapte a tu nivel y preferencia.</p>
+        <h2 className='pt-12 pb-4 text-2xl text-center font-display'>Torneos Organizados por Profesionales</h2>
+        <p>Todos los torneos presentados en nuestra plataforma son organizados por establecimientos reputados, garantizando una experiencia de competencia bien estructurada y emocionante.</p>
+        <h2 className='pt-12 pb-4 text-2xl text-center font-display'>Inscripción Sencilla</h2>
+        <p>Registrarse en el torneo de tu elección es un proceso simple y directo. Con solo unos clics, tú y tu equipo estarán en camino a competir por la gloria en el campo.</p>
+        <h2 className='pt-12 pb-4 text-2xl text-center font-display'>¡No Te Pierdas la Acción!</h2>
+        <p>Mantente al tanto de los próximos torneos y asegúrate de no perderte la oportunidad de participar. Regístrate para recibir actualizaciones y serás uno de los primeros en saber cuando los nuevos torneos estén disponibles para inscripción.</p>
+      </article>
+    </section>
+  )
+}
+export default Torneos
