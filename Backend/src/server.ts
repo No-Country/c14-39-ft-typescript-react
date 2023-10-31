f7b56e0ab275595676170e06b132e3e5c63b6d82
Luis Angel <lguzman.58erb@outlook.com>
2023-10-30 22:30:21 -0500

    end

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -31,7 +31,10 @@ import cors from "cors";
 //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //   }),
 // );
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser())
