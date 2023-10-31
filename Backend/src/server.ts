1091c773da85b82a498b2defd6d2dcc6b0d8eab9
Luis Angel <lguzman.58erb@outlook.com>
2023-10-30 22:18:32 -0500

    update

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -8,29 +8,30 @@ import { specs } from "./doc/swaggerConfig";
 import cookieParser from "cookie-parser";
 // Ruta principal
 import indexRoutes from "../src/routes/indexRoutes";
-import { devOrigins, prodOrigins } from "./data/consts";
+// import { devOrigins, prodOrigins } from "./data/consts";
 
 // dependencias node
 const app: Express = express();
 import cors from "cors";
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
-const allowedOrigins = isProduction ? prodOrigins : devOrigins;
+// const allowedOrigins = isProduction ? prodOrigins : devOrigins;
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error(`${origin} not allowed by CORS`));
-      }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }),
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error(`${origin} not allowed by CORS`));
+//       }
+//     },
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   }),
+// );
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser())
