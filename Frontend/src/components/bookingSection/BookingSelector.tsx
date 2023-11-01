99fc5720ac61e33aafa1a6f62b2018e03bdc07f4
cavera <cavera.de@gmail.com>
2023-10-31 22:35:56 -0500

    Skeleton loaders

--- Frontend/src/components/bookingSection/BookingSelector.tsx
+++ Frontend/src/components/bookingSection/BookingSelector.tsx
@@ -1,45 +1,41 @@
-import { useContext, useState, useEffect } from "react";
-import { AppContext, BookData } from "../../context/appcontext";
-import { AuthContext } from "../../context/AuthContext";
+import { useContext, useState, useEffect } from 'react'
+import { AppContext, BookData } from '../../context/appcontext'
+import { AuthContext } from '../../context/AuthContext'
 
-import espaciosTiempo from "../../data/mockdata_timepo.json";
-import { Camp, Center, UserData } from "../../types/types";
+import espaciosTiempo from '../../data/mockdata_timepo.json'
+import { Camp, Center, UserData } from '../../types/types'
 
-import { Calendario } from "./Calendario";
-import { RowTiempo } from "../form/RowItem";
-import { CanchaSelector } from "./CanchaSelector";
-import { Stepper } from "./Stepper";
+import { Calendario } from './Calendario'
+import { RowTiempo } from '../form/RowItem'
+import { CanchaSelector } from './CanchaSelector'
+import { Stepper } from './Stepper'
 
-import instance from "../../api/axios";
+import instance from '../../api/axios'
 
-export function BookingSelector({
-  proveedor,
-}: {
-  proveedor: Center | undefined;
-}) {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-  const [selectedCancha, setSelectedCancha] = useState<Camp | null>(null);
-  const [show, setShow] = useState<boolean>(false);
+export function BookingSelector({ proveedor }: { proveedor: Center | undefined }) {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [selectedCancha, setSelectedCancha] = useState<Camp | null>(null)
+  const [show, setShow] = useState<boolean>(false)
 
-  const { saveBooking, bookingData } = useContext(AppContext);
-  const { user } = useContext(AuthContext);
+  const { saveBooking, bookingData } = useContext(AppContext)
+  const { user } = useContext(AuthContext)
 
   // const navigate = useNavigate();
 
-  const today = new Date();
-  const minDate = new Date(today);
-  const maxDate = new Date(today.getTime() + 14 * 24 * 60 * 60 * 1000);
+  const today = new Date()
+  const minDate = new Date(today)
+  const maxDate = new Date(today.getTime() + 14 * 24 * 60 * 60 * 1000)
 
   useEffect(() => {
-    show && bookingData && user && redirectMercadoPago(bookingData, user);
-  }, [bookingData, show, user]);
+    show && bookingData && user && redirectMercadoPago(bookingData, user)
+  }, [bookingData, show, user])
 
   function showDate(date: Date) {
-    setSelectedDate(date);
+    setSelectedDate(date)
   }
 
   function selectCancha(cancha: Camp) {
-    setSelectedCancha(cancha);
+    setSelectedCancha(cancha)
   }
 
   function ConfirmBooking(hora: number) {
@@ -49,14 +45,14 @@ export function BookingSelector({
       cancha: selectedCancha as Camp,
       // precio: proveedor?.price as number,
       hora,
-    });
-    setShow(true);
+    })
+    setShow(true)
     // navigate(ROUTES.CONFIRM);
   }
 
   const redirectMercadoPago = (data: BookData, user: UserData) => {
     instance
-      .post("/order", {
+      .post('/order', {
         fecha: data.fecha,
         hora: data.hora.toString(),
         precio: 500,
@@ -65,44 +61,46 @@ export function BookingSelector({
         user_id: user.id,
       })
       .then(({ data }) => {
-        window.location.href = data.initPoint;
+        window.location.href = data.initPoint
       })
-      .catch((error) => console.log(`${error}`));
-  };
+      .catch(error => console.log(`${error}`))
+  }
 
   return (
-    <article className="wrapper">
-      <div className="grid md:grid-cols-[auto_1fr_1fr]  content-start gap-4 p-4 md:p-8 rounded-[2rem] backdrop-filter backdrop-blur-[20px] w-full bg-white/60">
+    <article className='wrapper'>
+      <div className='grid md:grid-cols-[auto_1fr_1fr]  content-start gap-4 p-4 md:p-8 rounded-[2rem] backdrop-filter backdrop-blur-[20px] w-full bg-white/60'>
         <Stepper
           paso={3}
           total={3}
-          mensaje="Confirma fecha, cancha y hora."
-          overrideClasses="col-span-1 md:col-span-3"
+          mensaje='Confirma fecha, cancha y hora.'
+          overrideClasses='col-span-1 md:col-span-3'
         />
-        <div className="">
-          <Calendario minDate={minDate} maxDate={maxDate} showDate={showDate} />
+        <div className=''>
+          <Calendario
+            minDate={minDate}
+            maxDate={maxDate}
+            showDate={showDate}
+          />
         </div>
 
-        <div className="px-2 py-4 rounded-lg bg-white/60">
-          <div className="grid items-start w-full gap-2 md:w-auto md:grid-cols-2">
+        <div className='px-2 py-4 rounded-lg bg-white/60'>
+          <div className='grid items-start w-full gap-2 md:w-auto lg:grid-cols-2'>
             {selectedDate &&
-              proveedor?.list_sport_camps?.map((item) => (
+              proveedor?.list_sport_camps?.map(item => (
                 <CanchaSelector
                   canchaId={item._id}
                   selected={selectedCancha as Camp}
                   key={`cselector${item._id}`}
-                  onSelectCancha={(p) => selectCancha(p)}
+                  onSelectCancha={p => selectCancha(p)}
                 />
               ))}
           </div>
         </div>
 
-        <div className="flex flex-col gap-2">
-          <div className="text-xl md:text-3xl ">
-            {selectedCancha?.camp_type_id.sca_type_name}
-          </div>
+        <div className='flex flex-col gap-2'>
+          <div className='text-xl md:text-3xl '>{selectedCancha?.camp_type_id.sca_type_name}</div>
           {selectedCancha &&
-            espaciosTiempo.map((item) => (
+            espaciosTiempo.map(item => (
               <RowTiempo
                 key={item.hora}
                 title={String(`${item.hora}:00`)}
@@ -112,5 +110,5 @@ export function BookingSelector({
         </div>
       </div>
     </article>
-  );
+  )
 }
