0bb306ab7c0c5f49e08634dfff250084bf0ccdfb
Kevin Defalco <kevindefalco@gmail.com>
2023-11-03 11:05:44 -0300

    acomodamos funcion sendEmail en mail.controller

--- Backend/src/controllers/mail.controller.ts
+++ Backend/src/controllers/mail.controller.ts
@@ -7,36 +7,38 @@ const { MAIL_PASSWORD } = process.env;
 
 
 interface EmailRequest {
-  toEmail: string;
-  subject: string;
-  text: string;
+    toEmail: string;
+    subject: string;
+    text: string;
 }
 
 export const sendEmail = async (req: Request, res: Response) => {
     const { toEmail, subject, text }: EmailRequest = req.body;
 
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'reservatucampo@gmail.com',
-        pass: `${MAIL_PASSWORD}`
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'reservatucampo@gmail.com',
+            pass: `${MAIL_PASSWORD}`
         }
     });
 
 
-const mailOptions = {
-    from: 'reservatucampo@gmail.com',
-    to: toEmail,
-    subject: subject,
-    text: text
-};
+    const mailOptions = {
+        from: 'reservatucampo@gmail.com',
+        to: toEmail,
+        subject: subject,
+        text: text
+    };
 
-try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: 'Email enviado con éxito' });
-} catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Error al enviar el email' });
-}
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: 'Email enviado con éxito' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error al enviar el email' });
+    }
 
 }
