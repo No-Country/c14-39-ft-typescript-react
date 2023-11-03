dc3051384e3fce1f772e56634a5575b2ee9b7861
Kevin Defalco <kevindefalco@gmail.com>
2023-11-03 18:10:47 -0300

    agregamos dominio resertucampo V2

--- Backend/src/data/consts.ts
+++ Backend/src/data/consts.ts
@@ -1,2 +1,2 @@
-export const aceptedOrigins = ["https://app.reservatucancha.vercel.app", "http://localhost:5173", "https://reservatucampo.vercel.app/"];
+export const aceptedOrigins = ["https://app.reservatucancha.vercel.app", "http://localhost:5173", "https://reservatucampo.vercel.app"];
 export const webhookUrl = 'https://reservatucancha.vercel.app/api/webhook'
